a37b930792002d742d10d2d5b6618a0c6a4ec305
Siyabonga Mdunjana <mdunjana.kwanele@icloud.com>
2025-10-13 20:06:39 +0200

    classs for products

---
+++ Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDONT2030.Models
+{
+    public class Products
+    {
+        public int productId;
+        public string productName="";
+        public int supplierId;
+        public int categoryId;
+        public decimal unitPrice;
+        public bool discontinued;
+    }
+}
