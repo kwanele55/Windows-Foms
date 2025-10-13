b6a698166ee6003cb058de5c71821cd5c1784d6e
Siyabonga Mdunjana <mdunjana.kwanele@icloud.com>
2025-10-13 20:25:41 +0200

    added a method that allows to read the products from the database

--- Repositories/ProductsRepositories.cs
+++ Repositories/ProductsRepositories.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRUDONT2030.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,11 @@ namespace CRUDONT2030.Repositories
     public class ProductsRepositories
     {
         private readonly string connectionString = "Data Source=.;Initial Catalog=TSQL2025;Integrated Security=True;Trust Server Certificate=False";
+
+        public List<Products> GetAllProducts()  //method to get all products
+        {
+            var products = new List<Products>(); //list to hold products object
+            return products; //return the list which is currently empty
+        }
     }
 }
