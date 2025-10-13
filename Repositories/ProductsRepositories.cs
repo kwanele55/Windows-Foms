afa9e846b6f456291d25a80022d80f5e7c7a233c
Siyabonga Mdunjana <mdunjana.kwanele@icloud.com>
2025-10-13 20:14:43 +0200

    connection string to the database

---
+++ Repositories/ProductsRepositories.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDONT2030.Repositories
+{
+    public class ProductsRepositories
+    {
+        private readonly string connectionString = "Data Source=.;Initial Catalog=TSQL2025;Integrated Security=True;Trust Server Certificate=False";
+    }
+}
